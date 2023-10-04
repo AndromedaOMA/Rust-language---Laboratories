074f4cef98a99f6ee7ebb2339d68e3a95792e03f
AndromedaOMA <116078879+AndromedaOMA@users.noreply.github.com>
2023-10-04 14:59:58 +0300

    Am finalizat ultima problema

--- LAB_01/problem_3/src/main.rs
+++ LAB_01/problem_3/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
     let mut value = 99;
-    while value >= 1 {
-        if value == 1 {
+    while value >= 0 {
+        if value == 0 {
+            println!("No bottles of beer on the wall,");
+            println!("No bottles of beer.");
+            println!("Go to the store, buy some more,");
+            println!("99 bottles of beer on the wall.");
+        } else if value == 1 {
             println!("{value} bottles of beer on the wall,");
             println!("{value} bottles of beer.");
             println!("Take one down, pass it around");
             println!("No bottles of beer on the wall.");
+            println!("");
         } else {
             println!("{value} bottles of beer on the wall,");
             println!("{value} bottles of beer.");
