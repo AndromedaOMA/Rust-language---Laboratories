f277748f2792e737ab37f8efd91e2f98af8381ba
AndromedaOMA <116078879+AndromedaOMA@users.noreply.github.com>
2023-11-02 19:26:48 +0200

    update

--- LAB_04/problem_4/src/main.rs
+++ LAB_04/problem_4/src/main.rs
@@ -4,23 +4,24 @@ fn do_stuff() -> Result<(), io::Error> {
     let s = fs::read_to_string("C:/Windows/System32/drivers/etc/services")?;
     let mut index;
     let mut final_string = String::new();
-    for lines in s.lines() {
-        if lines.find('#').is_some() {
-            index = lines.find('#').unwrap();
+    for l in s.lines() {
+        if l.find('#').is_some() {
+            index = l.find('#').unwrap();
         } else {
-            println!("{lines}");
             index = 0;
         }
 
-        if lines.is_empty() {
+        if l.is_empty() {
             continue;
         } else if index != 0 {
-            for c in lines.splitn(3, ' ') {
-                final_string.push_str(c);
-                final_string.push_str(" => ");
+            let mut columns = l.split_whitespace();
+
+            if let Some(first_column) = columns.next() {
+                if let Some(second_column) = columns.next() {
+                    println!("{} => {}", first_column, second_column);
+                }
             }
         }
-        println!("{final_string}");
     }
     Ok(())
 }
