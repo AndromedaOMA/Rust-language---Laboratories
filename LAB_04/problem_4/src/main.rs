3b13f93097504512f0c3a4aabc408b40e9059f6b
AndromedaOMA <116078879+AndromedaOMA@users.noreply.github.com>
2023-11-02 18:55:59 +0200

    update

--- LAB_04/problem_4/src/main.rs
+++ LAB_04/problem_4/src/main.rs
@@ -1,16 +1,27 @@
 use std::{fs, io};
 
 fn do_stuff() -> Result<(), io::Error> {
-    let s = fs::read_to_string("/c/Windows/System32/drivers/etc/services")?;
-
-    for l in s.lines() {
-        if l.is_empty() {
-            continue;
-        } else if l.find('#').is_some() {
-            println!("{l}");
+    let s = fs::read_to_string("C:/Windows/System32/drivers/etc/services")?;
+    let mut index;
+    let mut final_string = String::new();
+    for lines in s.lines() {
+        if lines.find('#').is_some() {
+            index = lines.find('#').unwrap();
+        } else {
+            println!("{lines}");
+            index = 0;
         }
-    }
 
+        if lines.is_empty() {
+            continue;
+        } else if index != 0 {
+            for c in lines.splitn(3, ' ') {
+                final_string.push_str(c);
+                final_string.push_str(" => ");
+            }
+        }
+        println!("{final_string}");
+    }
     Ok(())
 }
 
