48d65285251dde83cf884ba122b8dc5d40a102b6
AndromedaOMA <116078879+AndromedaOMA@users.noreply.github.com>
2023-11-02 19:08:58 +0200

    update

--- LAB_04/problem_3/src/main.rs
+++ LAB_04/problem_3/src/main.rs
@@ -1,7 +1,17 @@
+use std::io::{Error, ErrorKind};
 use std::{fs, io};
 
 fn do_stuff() -> Result<(), io::Error> {
+    let custom_error = Error::new(ErrorKind::Other, "oh no!");
     let mut s = fs::read_to_string("src/Text.txt")?;
+
+    if s.find("pt").is_none()
+        && s.find("ptr").is_none()
+        && s.find("dl").is_none()
+        && s.find("dna").is_none()
+    {
+        return Err(custom_error);
+    }
 
     s = s.replace("pt", "pentru");
     s = s.replace("ptr", "pentru");
@@ -13,5 +23,5 @@ fn do_stuff() -> Result<(), io::Error> {
 }
 
 fn main() {
-    let _ = do_stuff();
+    print!("{:?}",do_stuff());
 }
