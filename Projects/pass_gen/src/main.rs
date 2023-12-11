341b8efcd64248e41cfa0d15ee41866963325980
AndromedaOMA <116078879+AndromedaOMA@users.noreply.github.com>
2023-12-11 14:16:40 +0200

    generated the random password

--- Projects/pass_gen/src/main.rs
+++ Projects/pass_gen/src/main.rs
@@ -5,16 +5,35 @@ fn main() {
     //source: https://www.youtube.com/watch?v=AxPDP9Dj08U
     //and: https://docs.rs/fastrand/1.3.0/fastrand/
     let random_length = fastrand::usize(12..19);
+    
     // let password: String = repeat_with(fastrand::alphanumeric).take(random_length).collect();
 
-    let list_of_chars:Vec<char>= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':,./<>?".chars().collect();
+    //============================================LISTS OF CHARS==========================================
+    let list_of_alphanumeric: Vec<char> =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            .chars()
+            .collect();
+    let list_of_special_chars: Vec<char> = "!?#@".chars().collect();
+    let list_of_uppercase: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
+    //====================================================================================================
 
+
+    //============================================RANDOM PASSWORD==========================================
     let password: String = (0..random_length)
-        .map(|_| {
-            let random_index = fastrand::usize(..list_of_chars.len());
-            list_of_chars[random_index]
+        .map(|current_index| {
+            if current_index == 0 {
+                let random_index = fastrand::usize(..list_of_uppercase.len());
+                list_of_uppercase[random_index].to_string()
+            } else if current_index == 1 {
+                let random_index = fastrand::usize(..list_of_special_chars.len());
+                list_of_special_chars[random_index].to_string()
+            } else {
+                let random_index = fastrand::usize(..list_of_alphanumeric.len());
+                list_of_alphanumeric[random_index].to_string()
+            }
         })
         .collect();
+    //====================================================================================================
 
     println!("Hello!!");
     println!("-> Here is the random password: {}", password);
